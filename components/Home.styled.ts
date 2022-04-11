618b32a8c0c91b5fe119121dcecf875f1a6fad43
Felipe Vieira Lima <felipe.lima@bancointer.com.br>
2022-04-11 17:12:16 -0300

    Header with login raiwbowkit web3

---
+++ components/Home.styled.ts
@@ -0,0 +1,13 @@
+import { styled } from '@stitches/react'
+
+export const Container = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  padding: '8px 16px',
+  border: '1px solid #eee'
+})
+
+export const H1 = styled('h1', {
+  fontSize: '24px',
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,6 @@
 import type { NextPage } from 'next'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { styled } from '@stitches/react'
-
-const Container = styled('div', {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  padding: '8px 16px',
-  border: '1px solid #eee'
-})
-
-const H1 = styled('h1', {
-  fontSize: '24px',
-})
+import { Container, H1 } from '../components/Home.styled'
 
 const Home: NextPage = () => {
   return (
